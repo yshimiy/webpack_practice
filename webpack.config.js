5844dd1a30a51cbd7d899324a308c0c8e63a1cab
yshimiy <89027049+yshimiy@users.noreply.github.com>
2021-08-20 21:37:30 +0900

    20210820 Entract CSS & HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'main.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ],
-            },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          }
         ],
-    },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
 };
